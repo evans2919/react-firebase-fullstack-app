1d76e8cf6449164fb1c9521441035fb4ef61ce26
evans2919 <evansalcantara29@outlook.com>
2023-10-18 17:49:03 +0200

    Add conditional visualization

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,7 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useUserContext } from "../../context/UserContext";
 
 const Layout = () => {
+    const { user } = useUserContext();
+    if (user === false) {
+        return <p>loading...</p>;
+    }
     return (
         <>
             <header>
