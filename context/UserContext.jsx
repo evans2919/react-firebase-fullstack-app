3067a96bb9be3f27b4590542be417ec399735790
evans2919 <evansalcantara29@outlook.com>
2023-10-18 17:46:47 +0200

    Add firebase auth user

--- context/UserContext.jsx
+++ context/UserContext.jsx
@@ -1,22 +1,51 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { createContext, useContext, useState } from "react";
+import {
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+} from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../src/firebase";
 
 export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(false);
 
-    const singIn = () => {
-        setUser(true);
-    };
+    // Recuperar información de usuario activo
+    useEffect(() => {
+        const unsuscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const { email, photoURL, displayName, uid } = user;
+                setUser({ email, photoURL, displayName, uid });
+                console.log(user);
+            } else {
+                setUser(null);
+            }
+        });
+        return () => unsuscribe();
+    }, []);
 
-    const singOut = () => {
-        setUser(false);
-    };
+    // registrar usuario
+
+    const registerUser = (email, password) =>
+        createUserWithEmailAndPassword(auth, email, password);
+
+    // Iniciar sesión
+
+    const loginUser = (email, password) =>
+        signInWithEmailAndPassword(auth, email, password);
+
+    // Cerrar sesión
+
+    const singOutUser = () => signOut(auth);
 
     return (
-        <UserContext.Provider value={{ user, singIn, singOut, setUser }}>
+        <UserContext.Provider
+            value={{ user, setUser, loginUser, registerUser, singOutUser }}
+        >
             {children}
         </UserContext.Provider>
     );
