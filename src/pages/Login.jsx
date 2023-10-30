9948cd55a6c61ff51d3a7b09f3e52f4dfc359ae8
evans2919 <evansalcantara29@outlook.com>
2023-10-30 10:28:50 +0100

    Refactor Login Form & add useForm hook

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,39 +1,61 @@
 import { useNavigate } from "react-router-dom";
 import { useUserContext } from "../../context/UserContext";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import FormInput from "../components/FormInput";
+import FormError from "../components/utils/FormError";
+import { FirebaseErrors } from "../components/utils/FirebaseErrors";
+import { FormValidations } from "../components/utils/FormValidations";
 
 const Login = () => {
     const { loginUser } = useUserContext();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const navigate = useNavigate();
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
+    const { patternEmail, required, validateTrim, validateMinLength } =
+        FormValidations();
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = async ({ email, password }) => {
         try {
             await loginUser(email, password);
             navigate("/");
-            console.log("correcto");
         } catch (error) {
             console.log(error.code);
+            const { code, message } = FirebaseErrors(error);
+            setError(code, { message });
         }
     };
+
     return (
         <>
             <h1>Login</h1>
-            <form action="" onSubmit={handleSubmit}>
-                <input
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <FormInput
                     type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
                     placeholder="Ingrese email"
+                    {...register("email", {
+                        required,
+                        pattern: patternEmail,
+                    })}
                 />
-                <input
+                {errors.email && <FormError errors={errors.email.message} />}
+
+                <FormInput
                     type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
                     placeholder="Ingrese contraseña"
+                    {...register("password", {
+                        minLength: validateMinLength(6, "Mínimo 6 caracteres"),
+                        validate: {
+                            trim: validateTrim,
+                        },
+                    })}
                 />
+                {errors.password && (
+                    <FormError errors={errors.password.message} />
+                )}
+
                 <button type="submit">Login</button>
             </form>
         </>
