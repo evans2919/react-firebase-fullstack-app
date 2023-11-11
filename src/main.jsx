6e9094a1edd05a8a7f69a0180d0e6ecb9992d2a8
evans2919 <evansalcantara29@outlook.com>
2023-11-11 11:22:28 +0100

    Install tailwind and flowbite

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import router from "./router";
 import { RouterProvider } from "react-router-dom";
 import UserProvider from "../context/UserContext";
+import "flowbite"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
