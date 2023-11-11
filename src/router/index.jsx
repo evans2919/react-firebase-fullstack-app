f5993d12b58835bc7547733002cddbe01e1f854f
evans2919 <evansalcantara29@outlook.com>
2023-11-11 11:26:32 +0100

    Add new route for Layout Form

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -5,38 +5,53 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import RequireAuth from "../components/RequireAuth";
 import Register from "../pages/Register";
+import LayoutForm from "../Layout/LayoutForm";
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <Layout />,
         errorElement: <NotFound />,
         children: [
             {
                 errorElement: <NotFound />,
                 children: [
                     {
-                        index: true,
-                        element: (
-                            <RequireAuth>
-                                <Home />
-                            </RequireAuth>
-                        ),
-                    },
-
-                    {
-                        path: "login",
-                        element: <Login />,
-                    },
-                    {
-                        path: "register",
-                        element: <Register />,
+                        path: "/",
+                        element: <Layout />,
+                        children: [
+                            {
+                                index: true,
+                                element: (
+                                    <RequireAuth>
+                                        <Home />
+                                    </RequireAuth>
+                                ),
+                            },
+                            {
+                                path: "*",
+                                element: <NotFound />,
+                            },
+                        ],
                     },
                 ],
             },
             {
-                path: "*",
-                element: <NotFound />,
+                errorElement: <NotFound />,
+                children: [
+                    {
+                        path: "/",
+                        element: <LayoutForm />,
+                        children: [
+                            {
+                                path: "login",
+                                element: <Login />,
+                            },
+                            {
+                                path: "register",
+                                element: <Register />,
+                            },
+                        ],
+                    },
+                ],
             },
         ],
     },
