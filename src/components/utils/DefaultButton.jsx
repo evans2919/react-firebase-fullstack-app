9b05b235dda7e493f1b5dfc682957922c849598f
evans2919 <evansalcantara29@outlook.com>
2023-11-18 20:38:31 +0100

    Add new styles

--- src/components/utils/DefaultButton.jsx
+++ src/components/utils/DefaultButton.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-const DefaultButton = ({ buttonTitle, buttonStyle }, props) => {
+const DefaultButton = ({ buttonTitle, buttonStyle, children, ...props }) => {
     let style = "";
     switch (buttonStyle) {
         case "green":
@@ -18,6 +18,10 @@ const DefaultButton = ({ buttonTitle, buttonStyle }, props) => {
             style =
                 "focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 my-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900";
             break;
+        case "burguer":
+            style =
+                "inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600";
+            break;
 
         default:
             style =
@@ -28,7 +32,9 @@ const DefaultButton = ({ buttonTitle, buttonStyle }, props) => {
     return (
         <div>
             <button {...props} className={style}>
-                {buttonTitle}
+                <span>{buttonTitle}</span>
+
+                {children}
             </button>
         </div>
     );
